10c53cce586db2f68f541270c58fbfcf0edca4d1
Naelly4 <130795205+Naelly4@users.noreply.github.com>
2023-04-18 10:34:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-"""from Tela.tela import Ui_MainWindow
+from Tela.tela import Ui_MainWindow
 from PyQt5 import QtWidgets
 import sys
 import os
@@ -18,9 +18,46 @@ if __name__ == "__main__":
     aplicativo = App()  
     aplicativo.show()
     sys.exit(app.exec_())
+             
+             
+             
+    
+    '''
+    class CadastroVeiculos(QtWidgets.QMainWindow):
+    def __init__(self, *args, **argvs):# Metodo construtor da aplicação
+        super(CadastroVeiculos, self).__init__(*args, **argvs)
+        self.ui = Ui_cadastro_Veiculos()
+        self.ui.setupUi(self)
+        self.dataDeHoje = datetime.datetime.now()
+        self.ui.dateEdit.setDate(self.dataDeHoje)
+        self.ui.pushButton_3.clicked.connect(self.set_cadastrarnovoveiculo)
+        self.log = logging.getLogger(__name__)
+        self.log.debug("Inicializando Cadastro de veiculos")
+        self.db = DataBase()
+        #self.catalogarveiculos()
+        self.ui.pushButton_2.clicked.connect(self.pesquisarveiculo)'''
+    
+    
+     def set_cadastrar_cliente(self):
+        try:
+            nome = self.ui.lineEdit.text()
+            CPF  =  self.ui.lineEdit_2.text()
+            email =  self.ui.lineEdit_3.text()
+            numero_telefone = self.ui.lineEdit_5.text()
+             = self.ui.lineEdit_6.text()
+            = self.ui.lineEdit_6.text()
+            = self.ui.lineEdit_6.text()
+            = self.ui.lineEdit_6.text()
+    
+    
+    
+    
+    
+    
+    
 
-"""
 
+'''
 class Forma():
 
     def __init__(self, leite, farinha , ovos, fermento_em_po, achocolatado) -> bool:
@@ -43,4 +80,4 @@ class Forma():
         print(self.retorno)
 
 bolo = Forma(achocolatado='sa', farinha='sa', fermento_em_po='s', leite='sa', ovos='sa')
-bolo.set_validar_ingredientes()
+bolo.set_validar_ingredientes()'''
